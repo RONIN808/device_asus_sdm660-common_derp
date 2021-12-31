rishav-r3/device_xiaomi_sdm660-common

sdm660-common: add display config file
 * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
 * DisplayDeviceConfig: requesting nits when no mapping exists.

Signed-off-by: orges <me@orgesified.ml>

sdm660.mk
@@ -202,6 +202,10 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     vendor.display.config@1.1
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # DPM
 PRODUCT_PACKAGES += \
     libshim_dpmframework

RONIN808/device_asus_sdm660-common_derp

Change-Id: I1716319bb17b9236d07d44a2a7fa3c7640c54ddf

sdm660.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Doze
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \